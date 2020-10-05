8a06ecb19351ebc409a73f30fbbd77eaf82bb05f
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2020-10-05 11:09:36 +0100

    Fill out setup.py (with requirements etc.)

--- setup.py
+++ setup.py
@@ -4,4 +4,17 @@ from setuptools import setup
 
 setup(name = 'csat2',
       version = '0.1',
+      author='Edward Gryspeerdt',
+      author_email='e.gryspeerdt@imperial.ac.uk',
+      maintainer='Edward Gryspeerdt',
+      maintainer_email='e.gryspeerdt@imperial.ac.uk',
+      description='Python library for satellite and meteorological data ',
+      license='Not currently open source',
+      install_requires=['xarray',
+                        'numpy',
+                        'scipy',
+                        'netCDF4',
+                        'matplotlib',
+                        'scikit-learn',
+                        'google-cloud-storage'],
       packages = ['csat2'])
