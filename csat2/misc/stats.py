Fixed bug where reduce res was always factor 4...

csat2/misc/stats.py
@@ -79,7 +79,7 @@ def lat_weighted_av(data, latrange, lat_axis=0):
 def reduce_res(data, res, axis=(0, 1), func=np.ma.mean):
     '''Reduces the resolution along axis. Currently only the first two dimensions'''
     dshape = data.shape
-    newdshape = [dshape[0]//4, 4, dshape[1]//4, 4]
+    newdshape = [dshape[0]//res, res, dshape[1]//res, res]
     return func(data.reshape(newdshape), axis=(1, 3))
 
 