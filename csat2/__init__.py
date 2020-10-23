7e5782d40ab3a6bde2e7ec64f8f3211198ebe7b3
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2020-10-23 14:04:03 +0100

    Set canonical version location

--- csat2/__init__.py
+++ csat2/__init__.py
@@ -1,11 +1,6 @@
-import logging
+from ._version import __version__
 from .locator import configloader
 import os
-
-version = '0.1'
-
-# Set logging level
-logging.basicConfig(level=logging.ERROR)
 
 # user_location - the default location for the user override file
 user_location = os.path.expanduser('~/.csat2/config.cfg')
--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python3
 
 from setuptools import setup
+import re
+
+# Version code from https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+VERSIONFILE = "csat2/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 
 setup(name = 'csat2',
-      version = '0.1',
+      version = verstr,
       author='Edward Gryspeerdt',
       author_email='e.gryspeerdt@imperial.ac.uk',
       maintainer='Edward Gryspeerdt',
