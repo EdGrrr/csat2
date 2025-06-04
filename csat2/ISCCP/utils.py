e1bd68071d1e27a323089f2516ac74b50d15c46f
“geoffreypugsley” <gjpugsley@gmail.com>
2025-06-04 11:52:07 +0100

    ensure years are within the valid range for the data (1983 - 2017), make this more dynamic in the future

--- csat2/ISCCP/utils.py
+++ csat2/ISCCP/utils.py
@@ -9,6 +9,12 @@ def check_valid_args( year: int, doy: int, time: int,product: str, version: str)
     valid_times = range(0, 24, 3)
 
     max_doy = 366 if calendar.isleap(year) else 365
+
+    min_year, max_year = 1983, 2017 ## TODO: make this more dynamic incase more data is added
+
+    if not (min_year <= year <= max_year):
+        raise ValueError(f"Year must be between {min_year} and {max_year}, got {year}.")  
+
     if not (1 <= doy <= max_doy):
         raise ValueError(f"Day-of-year (doy) must be between 1 and {max_doy} for year {year}, got {doy}.")
     
